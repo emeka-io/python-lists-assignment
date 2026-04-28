9a411aafd59245e8f31e5c46f247e283e18ef848
Chukwuemeka Ifeanyi <theemekamail@gmail.com>
2026-04-28 06:37:32 -0700

    Modify colors in the list
    Updated specific colors in the list and changed the first item.

---
+++ task5_mutation.py
@@ -0,0 +1,12 @@
+colors = ["Red", "Blue", "Green", "Yellow", "Black"]
+
+# Update specific colors
+colors[1] = "Purple" # Change Blue
+colors[3] = "White"  # Change Yellow
+colors[4] = "Silver" # Change Black
+
+print("Updated list:", colors)
+
+# Change the first item
+colors[0] = "Crimson"
+print("Final list:", colors)
