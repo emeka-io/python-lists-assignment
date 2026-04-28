953db33b2d4e7165a6946b7fba67b21aaea6be6d
Chukwuemeka Ifeanyi <theemekamail@gmail.com>
2026-04-28 06:41:31 -0700

    Add bonus.py with number slicing challenges

---
+++ bonus.py
@@ -0,0 +1,11 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 1. Produce [10, 8, 6, 4, 2]
+# Start at the end, step backwards by 2
+challenge_1 = nums[::-2]
+print("Bonus 1:", challenge_1)
+
+# 2. Produce [3, 4, 5, 6, 7]
+# Slice from index 2 to 7
+challenge_2 = nums[2:7]
+print("Bonus 2:", challenge_2)
