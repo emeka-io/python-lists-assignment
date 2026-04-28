51f53b4b0ea21cc9329451f4ac58ee59a791be0f
Chukwuemeka Ifeanyi <theemekamail@gmail.com>
2026-04-28 06:36:19 -0700

    Add negative indexing examples in task3_negative_indexing.py

---
+++ task3_negative_indexing.py
@@ -0,0 +1,7 @@
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+# Using ONLY negative indexes
+print("Last number:", numbers[-1])
+print("First number:", numbers[-9])
+print("Fourth from last:", numbers[-4])
+print("Second from last:", numbers[-2])
