9fbc798d202ab25c5f61524126e3107dfb8f06bb
Chukwuemeka Ifeanyi <theemekamail@gmail.com>
2026-04-28 06:36:57 -0700

    Add slicing examples for list manipulation

---
+++ task4_slicing.py
@@ -0,0 +1,8 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print("First 3 items:", nums[:3])
+print("Last 3 items:", nums[-3:])
+print("Middle 4 items:", nums[3:7])
+print("Even-positioned elements (indices 0, 2, 4...):", nums[::2])
+print("Odd-positioned elements (indices 1, 3, 5...):", nums[1::2])
+print("Reversed list:", nums[::-1])
